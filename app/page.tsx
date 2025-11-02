b6aace4a6c05cf1848ce70aec75a144f708c9c2f
Halim Choukani <halim.choukani@gmail.com>
2025-11-02 23:31:59 +0100

    Reseting Base URL to deployment URL

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database";
-import { events } from "@/lib/constants";
 import { cacheLife } from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
@@ -9,8 +8,8 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const Page = async () => {
   "use cache";
   cacheLife("hours");
-  //const response = await fetch(`${BASE_URL}/api/events`);
-  //const { events } = await response.json();
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
   return (
     <section className="">
       <h1 className="text-center">
