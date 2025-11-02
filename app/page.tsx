98d7ec286ec7ba5892293864f285e442e901d2ad
Halim Choukani <halim.choukani@gmail.com>
2025-11-02 23:51:45 +0100

    fixing main page events

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
+import EventCard from "@/components/EventCard";
 import { IEvent } from "@/database";
 import { cacheLife } from "next/cache";
 
@@ -10,28 +10,33 @@ const Page = async () => {
   cacheLife("hours");
   const response = await fetch(`${BASE_URL}/api/events`);
   const { events } = await response.json();
+
   return (
-    <section className="">
+    <section>
       <h1 className="text-center">
         The Hub for Every Dev <br /> Event You Can't Miss
       </h1>
       <p className="text-center mt-5">
-        Hackathons, Meetups, and Confrences, All in One Place
+        Hackathons, Meetups, and Conferences, All in One Place
       </p>
 
       <ExploreBtn />
 
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
+
         <ul className="events">
           {events &&
             events.length > 0 &&
             events.map((event: IEvent) => (
-              <EventCard key={event.slug} {...event} />
+              <li key={event.title} className="list-none">
+                <EventCard {...event} />
+              </li>
             ))}
         </ul>
       </div>
     </section>
   );
 };
+
 export default Page;
